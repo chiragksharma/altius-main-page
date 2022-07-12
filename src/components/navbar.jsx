fdae8c86c3025863f6090ba0d62e3a7721632adb
Chirag Sharma <chiragksharma15@gmail.com>
2022-07-12 23:05:15 +0530

    First message

---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import '../css/navbar.css';
+function Navbar()  {
+    return (
+        <div>
+            <div className="navbox">
+            <img src="/Altius_Logo.png" alt="Logo" className="Logo" />
+            </div>
+        </div>
+    )
+
+}
+
+export default Navbar;
